9280238f90f463529402bdbfb7d1afa3ff500390
Lydia Robnik <84912419+LydiaRobnik@users.noreply.github.com>
2022-03-23 10:23:13 +0100

    upload is working

---
+++ src/App.jsx
@@ -0,0 +1,106 @@
+import "./App.css";
+import React, { useEffect, useState, useRef } from "react";
+
+export default function App() {
+  const [data, setData] = useState();
+  const [randomPicture, setRandomPicture] = useState();
+  const [topText, setTopText] = useState("");
+  const [bottomText, setBottomText] = useState("");
+  const [memeText, setMemeText] = useState({ top: "", bottom: "" });
+  const [mounted, setMounted] = useState(false);
+  const [userFiles, setUserFiles] = useState({selectedFile: null});
+  const finalMeme = useRef(null);
+
+  useEffect(() => {
+    let isLoading = true;
+
+    const fetchData = async () => {
+      const response = await fetch("https://api.imgflip.com/get_memes");
+      const json = await response.json();
+      if (isLoading) {
+        setData(json.data.memes);
+        setMounted(true);
+      }
+    };
+    fetchData().catch(console.error);
+    return () => (isLoading = false);
+  }, []);
+
+  const useUserUploadAsMemePic = () => {
+    const objectURL = URL.createObjectURL(userFiles.selectedFile);
+    setRandomPicture(objectURL)
+  }
+
+  return (
+    <div className="App">
+      <h1>Create your own memes</h1>
+      <div className="memegenerator">
+        <div className="memeWrapper">
+          {mounted && (
+            <>
+              <img
+                src={randomPicture ? randomPicture : data[0].url}
+                alt="meme"
+              />
+            </>
+          )}
+          <h2 className="textTop">{memeText.top}</h2>
+          <h2 className="textBottom">{memeText.bottom}</h2>
+        </div>
+        <div className="playground">
+          <div className="userInput actionButtons">
+            <p>1. Choose or upload a picture</p>
+            <button
+              style={{ backgroundColor: "#5edb89" }}
+              onClick={() => {
+                let number = Math.floor(Math.random() * 100);
+                setRandomPicture(data[number].url);
+              }}
+            >
+              Random picture
+            </button>
+            <p className="upload">Upload your own pictures</p>
+            <input onChange={(e) => setUserFiles({selectedFile: e.target.files[0]})} type="file" id="input" accept="image/*" multiple/>
+            <button onClick={useUserUploadAsMemePic} style={{ backgroundColor: "#bbdcgf" }}>Upload</button>
+            <button style={{ backgroundColor: "#bbdcfa" }}>Download</button>
+          </div>
+          <div className="userInput">
+            <p>2. Enter your text here:</p>
+            <input
+              onChange={(e) => setTopText(e.target.value)}
+              type="text"
+              placeholder="top text"
+              value={topText}
+            />
+            <input
+              onChange={(e) => setBottomText(e.target.value)}
+              type="text"
+              placeholder="bottom text"
+              value={bottomText}
+            />
+            <button
+              onClick={() => {
+                setMemeText({ top: topText, bottom: bottomText });
+              }}
+            >
+              Add your text
+            </button>
+          </div>
+          <div className="userInput actionButtons">
+            <button
+              onClick={() => {
+                setTopText("");
+                setBottomText("");
+                setMemeText({ top: "", bottom: "" });
+                setRandomPicture();
+              }}
+              style={{ backgroundColor: "#FF865E" }}
+            >
+              Reset everything
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
